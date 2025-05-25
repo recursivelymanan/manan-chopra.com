4703e7987418f661a7785ac3c3a1ea14cec0925a
Manan <manan.zchopra@gmail.com>
2025-05-24 17:59:43 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import MainPage from "./MainPage";
+
+const App = () => {
+  return <MainPage />;
+};
+
+export default App;
---
+++ src/MainPage.tsx
@@ -0,0 +1,9 @@
+const MainPage = () => {
+  return (
+    <div>
+      <p>This is the main page.</p>
+    </div>
+  );
+};
+
+export default MainPage;
