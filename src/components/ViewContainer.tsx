1fc26a717ae47abc4b438944e78a8bf0c4171ea8
Manan <manan.zchopra@gmail.com>
2025-05-29 15:59:35 -0700

    Fix animation between views

--- src/components/ViewContainer.tsx
+++ src/components/ViewContainer.tsx
@@ -26,14 +26,14 @@ const ViewContainer: React.FC<ViewContainerProps> = ({ page }) => {
   }
 
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
       <motion.div
         key={page}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{
-          duration: 0.4,
+          duration: 0.2,
           ease: "easeInOut",
         }}
       >
