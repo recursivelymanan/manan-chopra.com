27ace5c3895bfcf8666f15930bcb8864ec6da0f9
Manan <manan.zchopra@gmail.com>
2025-05-30 19:02:04 -0700

    Clarify default font size

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,17 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 import type { PageName } from "./types/types";
 import ViewContainer from "./components/ViewContainer";
 import NavBar from "./components/NavBar";
 
 import styles from "./styles/MainPage.module.css";
+import { useWindowSize } from "@react-hookz/web";
+import MobileNavBar from "./components/MobileNavBar";
+
+const BREAKPOINT = 1050;
 
 const MainPage = () => {
   const [view, setView] = useState<PageName>("Home");
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  const { width } = useWindowSize();
+
+  useEffect(() => {
+    setIsMobile(width <= BREAKPOINT);
+  }, [width]);
 
   return (
     <div className={styles.mainContainer}>
-      <NavBar view={view} setView={setView} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={`${isMobile}`}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+        >
+          {isMobile ? (
+            <MobileNavBar view={view} setView={setView} />
+          ) : (
+            <NavBar view={view} setView={setView} />
+          )}
+        </motion.div>
+      </AnimatePresence>
       <ViewContainer page={view} />
     </div>
   );
